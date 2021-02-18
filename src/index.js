3751bd12ce93d80cde15b8691d804cb168212b7c
Charlie-UWG <75844445+Charlie-UWG@users.noreply.github.com>
2021-02-18 23:37:22 +0900

    テンプレート文字列まで

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-import "./styles.css";
+/**
+ * 変数 const let
+ */
+// let val1 = "変数１です";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// val1 = "変数1は変更できます";
+// console.log(val1);
+
+// const val2 = "変数２";
+// console.log(val2);
+
+// // オブジェクトのプロパティは変更可能
+// const val3 = {
+//   name: "charlie",
+//   age: 58
+// };
+
+// val3.name = "Charlie";
+
+// console.log(val3);
+
+/**
+ * テンプレート文字列
+ */
+const name = "ちゃーりー";
+const age = 58;
+//従来の方法は
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message1);
+// テンプレート文字列を用いると
+const message2 = `私の名前は ${name}です。年齢は ${age}です。`;
+console.log(message2);
